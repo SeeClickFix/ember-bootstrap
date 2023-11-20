fe95c913e455af24dc901be207690c387a0d5d1a
Sander Knauff <sanderknauff@hotmail.com>
2023-11-20 15:43:14 +0100

    Refactor bs-alert to remove @localCopy and avoid mutation after consumption error in Ember canary (#2027)
    * Refactor bs-alert to remove @localCopy
    
    The `@localCopy` decorate caused issues on Ember canary builds.
    By refactoring this component to use pure Ember reactivity we can get
    rid of the decorator.
    
    * Allow re-enabling of alert using property after hiding it by clicking

--- tests/integration/components/bs-alert-test.js
+++ tests/integration/components/bs-alert-test.js
@@ -150,6 +150,34 @@ module('Integration | Component | bs-alert', function (hooks) {
     assert.dom('.alert').hasClass('alert-success', 'alert has type class');
   });
 
+  test('alert can be made visible when setting visible=true after manually dismissing it', async function (assert) {
+    this.set('visible', true);
+
+    this.set('handleOnDismissed', () => {
+      this.set('visible', false);
+    });
+
+    await render(hbs`<BsAlert
+      @type='success'
+      @fade={{false}}
+      @visible={{this.visible}}
+      @onDismissed={{this.handleOnDismissed}}
+    >
+      Test
+    </BsAlert>`);
+
+    assert.dom('.alert').exists('alert has alert class');
+    assert.dom('button.close').exists({ count: 1 }, 'alert has close button');
+
+    await click('button.close');
+    assert.dom('.alert').doesNotExist('alert has no alert class');
+    assert.dom('*').hasText('', 'alert has no content');
+
+    this.set('visible', true);
+
+    assert.dom('.alert').exists('alert has alert class');
+  });
+
   test('dismissing alert does not change public visible property (DDAU)', async function (assert) {
     this.set('visible', true);
     await render(hbs`<BsAlert @type="success" @visible={{this.visible}} @fade={{false}}>Test</BsAlert>`);
