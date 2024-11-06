0e7a63accaa789937ed4e83138554e7a563918d7
Gaurav Munjal <gaurav.munjal@civicplus.com>
2024-11-06 08:14:47 -0500

    update to Ember 3.28 with ember-cli-update

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,17 +39,17 @@ module.exports = {
     // node files
     {
       files: [
-        '.eslintrc.js',
-        '.prettierrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'index.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'tests/dummy/config/**/*.js',
-        'lib/**/*.js',
-        'node-tests/**/*.js',
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './index.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './tests/dummy/config/**/*.js',
+        './lib/**/*.js',
+        './node-tests/**/*.js',
       ],
       excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
       parserOptions: {
@@ -68,6 +68,11 @@ module.exports = {
       env: {
         mocha: true,
       },
+    },
+    {
+      // Test files:
+      files: ['tests/**/*-test.{js,ts}'],
+      extends: ['plugin:qunit/recommended'],
     },
   ],
 };
