803f2bea24e3ada7579453ac015081692258de02
Gaurav Munjal <gaurav.munjal@civicplus.com>
2024-11-06 08:03:27 -0500

    drop support for ember < 3.28

--- config/ember-try.js
+++ config/ember-try.js
@@ -9,33 +9,6 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0',
-            bootstrap: bootstrapVersion,
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.20',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.20.5',
-            bootstrap: bootstrapVersion,
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.0',
-            bootstrap: bootstrapVersion,
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
